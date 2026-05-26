098d8fbfb57d4b245ce1174e2dca210c48aa6b9f
unknown <skamthe2005@gmail.com>
2026-05-26 17:29:04 +0530

    bubble sort code

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+# bubble sort
+
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+
+arr=[20,40,10,15,30]
+bubble_sort(arr)
+print(arr)
